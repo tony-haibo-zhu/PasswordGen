841dddb0e6ede550db7399d37e58c1b8af482309
shimingxy <shimingxy@163.com>
2015-11-13 23:57:35 +0800

    init

---
+++ PasswordGenTest.java
@@ -0,0 +1,20 @@
+package com.connsec.crypto;
+
+public class PasswordGenTest {
+
+	public PasswordGenTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PasswordGen gen=new PasswordGen();
+		for(int i=1;i<100;i++){
+			System.out.println(gen.gen());
+			System.out.println(gen.gen(6));
+			System.out.println(gen.gen(2,2,2,2));
+		}
+		
+	}
+
+}
